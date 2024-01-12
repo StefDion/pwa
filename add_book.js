e66d31e8f380649879edd6d39c22d407db20033b
sdion <sdion@uco.fr>
2024-01-12 20:26:54 +0100

    registering a service worker

---
+++ add_book.js
@@ -0,0 +1,4 @@
+console.log('hello depuis add_book');
+
+const technonameField = document.querySelector('#titre');
+const btnAdd = document.querySelector('#add');
---
+++ sw.js
@@ -0,0 +1,3 @@
+console.log('Hello depuis le service worker');
+
+

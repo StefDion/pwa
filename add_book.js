7c7c53147d3606ecc1b23979dbc98f8642d22f7c
sdion <sdion@uco.fr>
2024-01-13 17:25:50 +0100

    serving a custom response when user is not online

--- add_book.js
+++ add_book.js
@@ -1,4 +1,28 @@
 console.log('hello depuis add_book');
 
-const technonameField = document.querySelector('#titre');
-const btnAdd = document.querySelector('#add');
+const livretitreField = document.querySelector('#livre-titre');
+const livreresumeField = document.querySelector('#livre-resume');
+const livreisbnField = document.querySelector('#livre-isbn');
+const addLivreForm = document.querySelector('#add-livre-form');
+
+addLivreForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const payload = {
+        titre: livretitreField.value,
+        resume: livreresumeField.value,
+        isbn: livreisbnField.value
+    }
+
+    fetch('http://localhost:3001/livres', { 
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(resp => {
+            console.log(resp);
+        })
+        .catch(err =>console.error);
+})

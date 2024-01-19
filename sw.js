e61fd092ffc1eb1e53925acdf7503103892e165a
sdion <sdion@uco.fr>
2024-01-19 10:39:31 +0100

    cleaning old cache instances

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const cacheName = 'biblio' + '1.1';
+const cacheName = 'biblio' + '1.2';
 
 self.addEventListener('install', (evt) => {
     console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
+    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);       
     const cachePromise = caches.open(cacheName).then(cache => {
         return cache.addAll([
             'index.html',
@@ -18,29 +19,27 @@ self.addEventListener('install', (evt) => {
     });
 
     evt.waitUntil(cachePromise);
+
 });
+
+
 
 self.addEventListener('activate', (evt) => {
     console.log(`sw activé à ${new Date().toLocaleTimeString()}`);    
+    let cacheCleanedPromise = caches.keys().then(keys => {
+		keys.forEach(key => {
+			if(key !== cacheName) {
+                return caches.delete(key);
+            }
+		});
+	});
+	evt.waitUntil(cacheCleanedPromise);
 });
 
-/* self.addEventListener('fetch', (evt) => {
-    console.log('fetch sur url', evt.request.url);
-});
 
-self.addEventListener('fetch', (evt) => {
-    if(!navigator.onLine) {
-        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
-        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Apllication en mode dégradé. Veuillez vous connecter</div>', headers));
-    }
-    console.log('sw intercepte la requête suivante via fetch', evt);
-    console.log('url interceptée', evt.request.url);
-});     */
-/* self.addEventListener('fetch', (evt) => {
-    console.log('sw intercepte la requête suivante via fetch', evt);
-    console.log('url interceptée', evt.request.url);
-});  */
+
 // self.addEventListener('fetch', (evt) => {
+
     //     if(!navigator.onLine) {
     //         const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
     //         evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Apllication en mode dégradé. Veuillez vous connecter</div>', headers));
@@ -59,14 +58,16 @@ self.addEventListener('fetch', (evt) => {
     // });
     
     // network first strategy
-/*     self.addEventListener('fetch', evt => {
+    self.addEventListener('fetch', evt => {
         evt.respondWith(
             fetch(evt.request).then( res => {
+               
                 // we add the latest version into the cache
                 caches.open(cacheName).then(cache => cache.put(evt.request, res));
                 // we clone it as a response can be read only once (it's like a one time read stream)
                 return res.clone();
             })
             .catch(err => caches.match(evt.request))
-        );
-    }); */
+            );
+        
+    });
